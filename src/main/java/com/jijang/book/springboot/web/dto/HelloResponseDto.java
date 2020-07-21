c85134abc1191db1423fcbea164c8d96a043497f
mz_jangjinik <mz_jangjinik@woowafriends.com>
2020-07-22 01:28:16 +0900

    --내용 롬복 플러그인 추가 jsonPath : Json 응답값을 필드별로 검증할 수 있는 메소드 사용함.
    --이슈
    인텔리제이에서 자바의 컴파일 버전이 계속 변경됨

---
+++ src/main/java/com/jijang/book/springboot/web/dto/HelloResponseDto.java
@@ -0,0 +1,12 @@
+package com.jijang.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+
+    private final String name;
+    private final int amount;
+}
